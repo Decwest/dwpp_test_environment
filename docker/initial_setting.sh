22e471b93c7bd1616acdb5a1fffba74edd60be1f
Decwest <fumiyaonishi1016@gmail.com>
2025-10-06 16:43:43 +0900

    :sparkles: add rolling environment

--- docker/initial_setting.sh
+++ docker/initial_setting.sh
@@ -9,3 +9,4 @@ export TURTLEBOT3_MODEL=waffle  # Iron and older only with Gazebo Classic
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/$ROS_DISTRO/share/turtlebot3_gazebo/models # Iron and older only with Gazebo Classic
 
 alias cb="colcon build --symlink-install"
+alias cc="colcon clean workspace"
